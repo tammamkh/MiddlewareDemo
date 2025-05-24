ae2723efcb969f437ed68c89849f0edbc7b8a319
Tammamkh <tammamkhalaf96@gmail.com>
2025-05-24 14:58:13 +0400

    Add .gitignore for .NET Core

--- obj/Debug/net9.0/MiddlewareDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/MiddlewareDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiddlewareDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959bfb6c3cfffb6601d268455fa9b6890a732cfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiddlewareDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiddlewareDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
